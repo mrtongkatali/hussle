e7dec22015e07514dbf70e3c1ec5e158735dd470
Leo Diaz <leoangelo.diaz@gmail.com>
2017-03-19 02:23:33 +0800

    Refactor UI

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -2,17 +2,23 @@
   div.row.task-component
     div(class="col s12 m12 l12")
       div.task-content
-        h4.center.deadline-title What's your #[span.strike deadliest] deadline today?
-        input.validate.center(type="text", placeholder="Enter your task here...",  @keyup.enter="onCreateTask", v-model="taskTitle")
+        div.row
+          div(class="col s12 m12 l12")
+            h4.center.deadline-title What's your #[span.strike deadliest] deadline today?
+        div.row
+          div(class="col s12 m12 l12")
+            input.validate.center(type="text", placeholder="Add your deadline here then hit 'Enter'!",  @keyup.enter="onCreateTask", v-model="taskTitle")
+          div(class="col s12 m12 l12")
+            draggable.dragArea.collection(v-if="hasTaskList", :list="taskList")
+              div.collection-item(v-for="e in taskList", :title="itemElTitle")
+                div.item-block.white-text
+                  div.title-area
+                    span.white-text() {{ e.title }}
+                  div.action-area
+                    span.time {{ getFormattedDate(e.timestamp) }}
+                div.clear
+                div.details Lorem ipsum
 
-        draggable.dragArea.collection(v-if="hasTaskList", :list="taskList")
-          div.collection-item(v-for="e in taskList", :title="itemElTitle")
-            div.item-block.teal-text
-              div.title-area
-                span.teal-text() {{ e.title }}
-              div.action-area
-                span.time {{ getFormattedDate(e.timestamp) }}
-            div.clear
 </template>
 
 <script>
